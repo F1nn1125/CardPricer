8ccadcc25ca917ab1500e2a665bd60047ca0da45
Finn <71734783+F1nn1125@users.noreply.github.com>
2023-10-03 14:16:54 +1300

    Basically Finished
    - Added sum of cards prices
    - Added Currency convertion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sf_price_fetcher import fetcher
+from currency_converter import CurrencyConverter
 
 def main():
     # Key = name, value = price
@@ -30,7 +31,36 @@ def main():
     # Create dataframe
     card_names_and_prices_dataframe = pd.DataFrame.from_dict(card_details, orient='index')
 
+    # Sum all prices and insert into dataframe
+    total_price = sum(card_details.values())
+
     print("\nDisclaimer: All prices are in USD at the moment")
     print(card_names_and_prices_dataframe)
+    print(f"Total price of cards: {total_price}")
+
+    # Main loop done
+
+    convert = input("Convert currency? (y/n) ").lower().strip() == "y"
+
+    if convert:
+        symbol = input("Please input the currency's symbol you want to convert to, (e.g NZD, EUR, AUD): ")
+
+        for key, value in card_details.items():
+
+            # Convert then round the price to 2dp
+            converted_price = CurrencyConverter(decimal=True).convert(value, 'USD', symbol)
+
+            # Update the dictionary
+            card_details.update({key : round(converted_price, 2)})
+
+        print("Disclaimer: Currencies might not be fully up to date/live prices")
+        # Update and print dataframe
+        card_names_and_prices_dataframe = pd.DataFrame.from_dict(card_details, orient='index')
+        print(card_names_and_prices_dataframe)
+
+        # Sum all prices and insert into dataframe
+        total_price = sum(card_details.values())
+        print(f"Total price of cards: {total_price}\n")
+
 
 main()
