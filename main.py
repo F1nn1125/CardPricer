34280149f02e67e5cc9f476733ecb13050f3050e
Finn <71734783+F1nn1125@users.noreply.github.com>
2023-10-03 13:46:53 +1300

    Implemented scryfall price fetcher
    - Implemented scryfall price fetcher library with error loop if card name does not exist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sf_price_fetcher import fetcher
 
 def main():
     # Key = name, value = price
@@ -10,8 +11,16 @@ def main():
         repeats = int(input("how many cards? "))
 
         for i in range(repeats):
-            card_name = input("Card name: ")
-            card_price = float(input("Card price (USD): "))
+            while True:
+                card_name = input("Card name: ").lower()
+
+                try:
+                    # If cannot fetch card price it will loop
+                    card_price = fetcher.get(card_name)
+                    break
+                except:
+                    print("Uhoh! It looks like your card doesn't exist! Please check your spelling and re-enter.")
+                    continue
 
             # update dict with name and price
             card_details.update({card_name : card_price})
@@ -21,6 +30,7 @@ def main():
     # Create dataframe
     card_names_and_prices_dataframe = pd.DataFrame.from_dict(card_details, orient='index')
 
+    print("\nDisclaimer: All prices are in USD at the moment")
     print(card_names_and_prices_dataframe)
 
 main()
