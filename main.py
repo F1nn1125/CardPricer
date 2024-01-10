e13bbdac2d459b175e8d70713cba0df2998eea2a
Finn <71734783+F1nn1125@users.noreply.github.com>
2024-01-10 18:57:42 +1300

    Export to file + General Improvements
    - If chosen, the data frame and total price will be exported to a text file with the current date.
    - Fixed some dumb formatting decisions I made in 55d88d00e08a649096ef0a6490b9c07268e8e358, pylint makes some dumb decisions.
    - Created a default error message that is used throughout the code.
    - Got rid of the exception specifications, if someone wants to make them work properly then be my guest but I find I catch more errors that I want to without the specification.
    - ```if convert``` now excepts "yes" as well.
    - apparently I can't spell data frame lol

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+from datetime import datetime
 import pandas as pd
 from sf_price_fetcher import fetcher
 from currency_converter import CurrencyConverter
+
+# Get date for file_name
+today = datetime.now()
+
+day = today.strftime("%d")
+month = today.strftime("%m")
+year = today.strftime("%Y")
 
 # Converts the currency
 def convert_currency(card_details, default_symbol):
     while True:
         try:
             symbol = input("Please input the currency's symbol "
-                        "you want to convert to, (e.g NZD, EUR, AUD): ").upper()
-            CurrencyConverter().convert(1, default_symbol, symbol) # Throw a test convert to check if symbol exists
+                "you want to convert to, (e.g NZD, EUR, AUD): ").upper()
+
+            CurrencyConverter().convert(
+                1,
+                default_symbol,
+                symbol) # Throw a test convert to check if symbol exists
+            
             break
-        except ValueError:
+        except:
             print("Please input a valid currency symbol!")
             continue
 
@@ -22,14 +35,28 @@ def convert_currency(card_details, default_symbol):
         # Update the dictionary
         card_details.update({key : round(converted_price, 2)})
 
-    # Update and print dataframe
-    card_names_and_prices_dataframe = pd.DataFrame.from_dict(
+    # Update and print data frame
+    card_names_and_prices_data_frame = pd.DataFrame.from_dict(
         card_details,
         orient='index',
         columns=[''])
 
-    # Sum all prices and insert into dataframe
-    return sum(list(card_details.values())), card_names_and_prices_dataframe, symbol
+    # Sum all prices and insert into data frame
+    return sum(list(card_details.values())), card_names_and_prices_data_frame, symbol
+
+# It writes to the file
+def write_to_file(data_frame, total_price_string):
+    file_name = f"card_price_list_{day}_{month}_{year}"
+    write_to = f"{file_name}.txt"
+    text_file = open(write_to, "w+")
+    text_file.write(f"{file_name}\n\n")
+
+    # Write in the data frame + total price
+    text_file.write(str(data_frame))
+    text_file.write(total_price_string)
+
+    # Close the text_file
+    text_file.close()
 
 # Main Routine
 def main():
@@ -40,18 +67,22 @@ def main():
     # Default currency
     default_symbol = 'USD'
 
+    # Default error message
+    error_message = "Please enter either y or n"
+
     while True:
         while True:
-            card_name = input("Card name (leave blank if done): ").lower()
-
             try:
+                card_name = input("Card name (leave blank if done): ").lower()
+
                 # If cannot fetch card price (or if card_name != "") it will loop
                 if card_name != "":
                     card_price = fetcher.get(card_name)
+
                 break
-            except TypeError:
+            except:
                 print("Uhoh! It looks like your card doesn't exist, "
-                      "please check your spelling and re-enter.")
+                    "please check your spelling and re-enter.")
                 continue
 
         # Checks if user meant to break loop
@@ -67,8 +98,8 @@ def main():
         float(card_price)
         card_details.update({card_name : card_price})
 
-    # Create dataframe and total_price
-    card_names_and_prices_dataframe = pd.DataFrame.from_dict(
+    # Create data frame and total_price
+    card_names_and_prices_data_frame = pd.DataFrame.from_dict(
         card_details,
         orient='index',
         columns=[''])
@@ -79,16 +110,38 @@ def main():
 
     symbol = default_symbol
 
-    convert = input("Convert currency? (y/n): ").lower().strip() == "y"
+    while True:
+        try:
+            convert = input("Convert currency? (y/n): ").lower()
+            break
+        except:
+            print(error_message)
+            continue
 
-    if convert:
-        total_price, card_names_and_prices_dataframe, converted_symbol = convert_currency(
+    if convert == "y" or convert == "yes":
+        total_price, card_names_and_prices_data_frame, converted_symbol = convert_currency(
             card_details,
             default_symbol)
 
         symbol = converted_symbol
 
-    print(card_names_and_prices_dataframe)
-    print(f"Total price of cards: {symbol}${total_price}\n")
+    total_price_string = f"\nTotal price of cards: {symbol}${total_price}"
+
+    print(card_names_and_prices_data_frame)
+    print(total_price_string)
+
+    # Writing to file stuff
+    while True:
+        try:
+            answer = input("Do you want to export a list of the prices? (y/n): ").lower()
+            break
+        except:
+            print(error_message)
+            continue
+
+    if answer == "y" or answer == "yes":
+        write_to_file(
+            card_names_and_prices_data_frame,
+            total_price_string)
 
 main()
